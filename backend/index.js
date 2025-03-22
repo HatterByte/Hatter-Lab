f768480f005148d0ba42ae007ec371e1f0283b77
Anubhav Chandak <anubhavchandak@gmail.com>
2025-03-23 03:56:05 +0530

    deployed links added

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,10 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:5173", // Frontend URL
+    origin: [
+      "http://localhost:5173",
+      "https://hatter-lab.onrender.com",
+    ], // Frontend URL
     credentials: true,
   })
 );
