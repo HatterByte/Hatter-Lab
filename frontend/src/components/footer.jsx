a3daebc2bfd651aa83d15884a356ecaafe8fa368
Anubhav Chandak <anubhavchandak@gmail.com>
2025-07-06 08:47:10 +0530

    Footer changes

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import icons8Linkedin from "../assets/icons8-linkedin.svg";
 import icons8Mail from "../assets/icons8-mail-50.png";
+import icons8Github from "../assets/icons8-github.svg";
 
 const Footer = () => {
   return (
@@ -17,8 +18,18 @@ const Footer = () => {
         <a href="mailto:hatterbyte@gmail.com" className="footer-link">
           <img src={icons8Mail} alt="Email" className="w-8 h-8" />
         </a>
+        <a
+          href="https://github.com/hatterbyte"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="footer-link"
+        >
+          <img src={icons8Github} alt="GitHub" className="w-8 h-8" />
+        </a>
       </div>
-      <div className="text-[#858585] text-sm">&copy; 2025 Made by Hatter Byte</div>
+      <div className="text-[#858585] text-sm">
+        &copy; 2025 Made by Hatter Byte
+      </div>
     </footer>
   );
 };
